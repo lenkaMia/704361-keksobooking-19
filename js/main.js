07b442fa3184588655135b3e67c6b0951766838c
Elena Samotaeva <elena.samotaeva@gmail.com>
2020-01-30 22:10:44 +0300

    Добавляет отрисовку пинов

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var ADS_QTY = 8;
-var MAX_PRICE = 10000;
+var MAX_PRICE = 1000000;
 var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var ROOMS = [1, 2, 3, 100];
 var GUESTS = [0, 1, 2, 3];
@@ -14,8 +14,9 @@ var PIN = {
   MIN_Y: 130,
   MAX_Y: 630
 };
-
 var map = document.querySelector('.map');
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var mapPins = map.querySelector('.map__pins');
 
 var getAvatarSrc = function (index) {
   return 'img/avatars/user0' + (index + 1) + '.png';
@@ -30,7 +31,7 @@ var getRandomElement = function (array) {
 };
 
 var getRandomLengthArray = function (array) {
-  return array.slice(0, Math.ceil(Math.random() * array.length));
+  return array.slice(0, Math.floor(Math.random() * array.length) + 1);
 };
 
 var generateAdsArray = function (adsQty) {
@@ -68,6 +69,27 @@ var generateAdsArray = function (adsQty) {
   return advertisment;
 };
 
-generateAdsArray(ADS_QTY);
+var renderPin = function (ad) {
+  var adElement = pinTemplate.cloneNode(true);
+
+  adElement.style.left = (ad.location.x - 25) + 'px';
+  adElement.style.top = (ad.location.y - 70) + 'px';
+  adElement.querySelector('img').src = ad.author.avatar;
+  adElement.querySelector('img').alt = ad.offer.title;
+
+  return adElement;
+};
+
+var createPins = function (array) {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < array.length; i++) {
+    fragment.appendChild(renderPin(array[i]));
+  }
+
+  mapPins.appendChild(fragment);
+};
+
+createPins(generateAdsArray(ADS_QTY));
 
 map.classList.remove('map--faded');
