9fd2eafb19c8dc60f77cf43c5721e60006568c6d
Elena Samotaeva <elena.samotaeva@gmail.com>
2020-02-28 10:51:32 +0300

    шеф, все пропало

--- js/main.js
+++ js/main.js
@@ -1,16 +1,1 @@
-'use strict';
 
-(function () {
-  var ADS_QTY = 8;
-  var ads = window.data.generate(ADS_QTY);
-
-  var activatePage = function () {
-    window.map.activate();
-    window.form.activate();
-    window.map.renderPins(ads);
-    window.form.setAddress(window.map.getCoords());
-  };
-
-  window.form.setAddress(window.map.getCoords());
-
-})();
